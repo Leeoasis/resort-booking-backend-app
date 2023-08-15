1d71d74c5fce2397a55085e14c0c0d21766bb99b
Leslie <Kooliesk@gmail.com>
2023-08-15 13:13:30 +0200

    validate booking model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,3 +1,9 @@
 class Booking < ApplicationRecord
   belongs_to :resort
+
+  validates :customer_name, presence: true
+  validates :contact_information, presence: true
+  validates :reservation_date, presence: true
+  validates :duration, presence: true, numericality: { greater_than: 0 }
+  validates :num_guests, presence: true, numericality: { greater_than: 0 }
 end
