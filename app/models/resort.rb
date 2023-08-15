9e908cf37f63abbc4a38cd6a77c55f7618982f42
Leslie <Kooliesk@gmail.com>
2023-08-15 13:13:43 +0200

    validate resort model

--- app/models/resort.rb
+++ app/models/resort.rb
@@ -1,3 +1,14 @@
 class Resort < ApplicationRecord
   has_many :bookings
+
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :description, presence: true
+  validates :phone_number, presence: true
+  validates :email, presence: true, format: { with: Devise.email_regexp }
+  validates :city, presence: true
+  validates :country, presence: true
+  validates :available_rooms, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :max_occupancy, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :base_price, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
