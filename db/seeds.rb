b321cd3d97847555053257c52c51ab3c73366ece
Leslie <Kooliesk@gmail.com>
2023-08-16 13:31:10 +0200

    fix rubocop errors

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,23 +8,22 @@
 
 # Create Resorts
 resort = Resort.create!(
-    name: "Resort Name",
-    address: "123 Main Street",
-    description: "A beautiful resort...",
-    phone_number: "123-456-7890",
-    email: "info@example.com",
-    city: "City Name",
-    country: "Country Name",
-    available_rooms: 100,
-    max_occupancy: 2,
-    base_price: 150.00
-  )
-  
-  # Create a Booking for the Resort and User
-  Booking.create!(
-    user: user,
-    resort: resort,
-    selected_date: "2023-08-16",
-    selected_city: "City Name"
-  )
-  
+  name: 'Resort Name',
+  address: '123 Main Street',
+  description: 'A beautiful resort...',
+  phone_number: '123-456-7890',
+  email: 'info@example.com',
+  city: 'City Name',
+  country: 'Country Name',
+  available_rooms: 100,
+  max_occupancy: 2,
+  base_price: 150.00
+)
+
+# Create a Booking for the Resort and User
+Booking.create!(
+  user:,
+  resort:,
+  selected_date: '2023-08-16',
+  selected_city: 'City Name'
+)
