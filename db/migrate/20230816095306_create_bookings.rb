c5ffab5cfb79a924198629a6f0f5599ee9c4b9e7
Leslie <Kooliesk@gmail.com>
2023-08-16 12:29:15 +0200

    fix rubocop errors

--- db/migrate/20230816095306_create_bookings.rb
+++ db/migrate/20230816095306_create_bookings.rb
@@ -1,8 +1,8 @@
 class CreateBookings < ActiveRecord::Migration[7.0]
   def change
     create_table :bookings do |t|
-      t.references :user, foreign_key: true 
-      t.references :resort, foreign_key: true 
+      t.references :user, foreign_key: true
+      t.references :resort, foreign_key: true
       t.date :selected_date
       t.string :selected_city
 
