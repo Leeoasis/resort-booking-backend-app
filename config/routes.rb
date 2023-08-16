c91560f2766fa5200e4016620c43bc463bb6abc6
Leslie <Kooliesk@gmail.com>
2023-08-16 13:03:44 +0200

    fix rubocop errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,3 @@
 Rails.application.routes.draw do
   devise_for :users
-
-  namespace :api do
-    namespace :v1 do
-      resources :bookings, only: %i[index show create update destroy]
-      resources :resorts
-    end
-  end
 end
