567bc976ce38c8de6873cb8ea7a3cd6c49efd8da
Lawrence Amoafo <rekenburg777@gmail.com>
2023-08-17 14:54:38 +0000

    Add user reference to resorts table and run migration to update schema file

--- db/schema.rb
+++ db/schema.rb
@@ -10,49 +10,52 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_230_816_095_306) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_17_141627) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'bookings', force: :cascade do |t|
-    t.bigint 'user_id'
-    t.bigint 'resort_id'
-    t.date 'selected_date'
-    t.string 'selected_city'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['resort_id'], name: 'index_bookings_on_resort_id'
-    t.index ['user_id'], name: 'index_bookings_on_user_id'
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "resort_id"
+    t.date "selected_date"
+    t.string "selected_city"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["resort_id"], name: "index_bookings_on_resort_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
-  create_table 'resorts', force: :cascade do |t|
-    t.string 'name'
-    t.string 'address'
-    t.text 'description'
-    t.string 'phone_number'
-    t.string 'email'
-    t.string 'city'
-    t.string 'country'
-    t.integer 'available_rooms'
-    t.integer 'max_occupancy'
-    t.decimal 'base_price'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "resorts", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.text "description"
+    t.string "phone_number"
+    t.string "email"
+    t.string "city"
+    t.string "country"
+    t.integer "available_rooms"
+    t.integer "max_occupancy"
+    t.decimal "base_price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_resorts_on_user_id"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'email', default: '', null: false
-    t.string 'encrypted_password', default: '', null: false
-    t.string 'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.datetime 'remember_created_at'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.string 'name'
-    t.index ['email'], name: 'index_users_on_email', unique: true
-    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "name"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key 'bookings', 'resorts'
-  add_foreign_key 'bookings', 'users'
+  add_foreign_key "bookings", "resorts"
+  add_foreign_key "bookings", "users"
+  add_foreign_key "resorts", "users"
 end
