16a8ca7d90167f9cb5a3f2f3669f38b6a806de77
Abbas Sarwar <abbassarwar100@gmail.com>
2023-08-18 12:56:04 +0500

    Validationes for Resort & Bookings

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,10 @@
 class Booking < ApplicationRecord
   belongs_to :resort
   belongs_to :user
+
+  #validations
+  validates :selected_date, presence: true
+  validates :selected_city, presence: true
+  validates :resort_id, presence: true
+  validates :user_id, presence: true
 end
--- app/models/resort.rb
+++ app/models/resort.rb
@@ -1,4 +1,16 @@
 class Resort < ApplicationRecord
   has_many :bookings
   belongs_to :user
+
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :description, presence: true
+  validates :phone_number, presence: true, numericality: { only_integer: true }
+  validates :email, presence: true
+  validates :country, presence: true
+  validates :city, presence: true
+  validates :available_rooms: true, numericality: {only_integer: true}
+  validates :max_occupancy: true, numericality: {only_integer: true}
+  validates :base_price: true, numericality: {only_integer: true}
+  validates :user_id: true, numericality: {only_integer: true}
 end
