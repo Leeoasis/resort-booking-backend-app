a5428483b42ce38a161add473e810201c81d272d
Abbas Sarwar <abbassarwar100@gmail.com>
2023-08-18 13:00:21 +0500

    Linter fixes & invalid syntix fixes

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,7 +2,7 @@ class Booking < ApplicationRecord
   belongs_to :resort
   belongs_to :user
 
-  #validations
+  # validations
   validates :selected_date, presence: true
   validates :selected_city, presence: true
   validates :resort_id, presence: true
--- app/models/resort.rb
+++ app/models/resort.rb
@@ -9,8 +9,8 @@ class Resort < ApplicationRecord
   validates :email, presence: true
   validates :country, presence: true
   validates :city, presence: true
-  validates :available_rooms: true, numericality: {only_integer: true}
-  validates :max_occupancy: true, numericality: {only_integer: true}
-  validates :base_price: true, numericality: {only_integer: true}
-  validates :user_id: true, numericality: {only_integer: true}
+  validates :available_rooms, presence: true, numericality: { only_integer: true }
+  validates :max_occupancy, presence: true, numericality: { only_integer: true }
+  validates :base_price, presence: true, numericality: { only_integer: true }
+  validates :user_id, presence: true, numericality: { only_integer: true }
 end
