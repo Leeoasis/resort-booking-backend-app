86dc93c0ca7d0dd0a47e9ebe308ac0330c6d6cbb
Leslie <Kooliesk@gmail.com>
2023-08-24 20:28:45 +0200

    removed unused validation

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,7 +3,6 @@ class Booking < ApplicationRecord
   belongs_to :resort
 
   # validations
-  validates :selected_date, presence: true
   validates :selected_city, presence: true
   validates :resort_id, presence: true
   validates :user_id, presence: true
