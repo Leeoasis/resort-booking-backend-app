f4266341f3eb11038e9a22d97afa63eec9920c7b
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-08-26 21:49:57 +0530

    Add Jwt_denylist model spec

---
+++ spec/models/jwt_denylist_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe JwtDenylist, type: :model do
+  describe "table_name" do
+    it "has the correct table name" do
+      expect(described_class.table_name).to eq('jwt_denylist')
+    end
+  end
+
+  describe "revocation strategy" do
+    it "uses the Denylist revocation strategy" do
+      expect(described_class.included_modules).to include(Devise::JWT::RevocationStrategies::Denylist)
+    end
+  end
+end
