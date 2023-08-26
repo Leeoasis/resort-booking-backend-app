dfd8bce95c3b99ed374aac0c6a4d454098ac9311
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-08-26 21:41:57 +0530

    Add booking model specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
          :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
   
   has_many :resorts
+  has_many :bookings
 
   validates :email, presence: true, length: { in: 1..100 }
   validates :password, presence: true, length: { in: 6..50 }
