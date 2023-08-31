c277b8176c53ca32707de88a04017bbe3bedc5f0
Abbas Sarwar <abbassarwar100@gmail.com>
2023-08-31 13:38:56 +0500

    Changes for deployment

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,4 +7,13 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
       max_age: 600
     end
+
+    allow do
+      origins 'https://resort-app-frontend.onrender.com'
+      resource '*',
+      headers: ["Authorization"],
+      expose: ["Authorization"],
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      max_age: 600
+    end
   end
